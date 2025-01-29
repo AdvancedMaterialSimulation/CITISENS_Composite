48fbfc9c13a85c9f8747197c288049d67822bd80
Jesus Oroya <41575404+djoroya@users.noreply.github.com>
2025-01-29 17:10:41 +0100

    modelos analiticos

---
+++ src/AnalyticalLayers/plt_thin.py
@@ -0,0 +1,42 @@
+# bar plot compare tau_exp and tau_pred
+from matplotlib import pyplot as plt
+def plt_thin(df_stats, tau_exp, tau_pred, tn_opt, tl_opt):
+    fig = plt.figure(figsize=(9, 4))
+    ax = fig.add_subplot(211)
+    offset = 0.15
+    # bar plot tau_exp and tau_pred in the same plot separated by a gap
+    xspan = range(len(df_stats.index))
+    # ax.bar([x-offset for x in xspan], tau_exp, width=offset, label='Experimental')
+    error = df_stats['t [mm]']['std'].values
+    ax.bar([x-offset for x in xspan], tau_exp, yerr=error, width=offset, label='Experimental', capsize=5)
+    ax.bar([x+offset for x in xspan], tau_pred, width=offset, label='Predicted')
+    ax.set_xticks(xspan);
+    ax.set_xticklabels(df_stats.index);
+    ax.legend()
+    # t [mm]
+    plt.ylabel('Espesor (mm)')
+    plt.grid()
+
+    # print in plot the values of optimal tn and tl
+    fz = 12
+    fd = {'color':'black','weight':'bold',"family":'serif'}
+    # in title 
+    plt.title(f' $t_n = {tn_opt:.2f} \\ [mm]$ |  $t_l ={tl_opt:.2f} \\ [mm]$', fontsize=fz,fontdict=fd)
+
+    ax = fig.add_subplot(212)
+    # relative error
+    error = 100*abs(tau_exp - tau_pred)/tau_exp
+    ax.bar(xspan, error, width=0.4)
+    # ylabel
+    plt.ylabel('Error relativo (%)')
+    # horizontal line at 5%
+    plt.axhline(5, color='r', linestyle='--',label='5%')
+    plt.grid()
+    # mean error horizontal line
+    mean_error = error.mean()
+    plt.axhline(mean_error, color='g', linestyle='--',label='Error medio = {:.2f}%'.format(mean_error))
+    ax.set_xticks(xspan);
+    ax.set_xticklabels(df_stats.index);
+    ax.legend()
+
+    plt.ylim(0, 20)
--- src/Composite/CreateLines/CreateLines.py
+++ src/Composite/CreateLines/CreateLines.py
@@ -1,33 +1,16 @@
-from .layers import LayerX, LayerY
-from .LayerSinX import LayerSinX
-from .LayerSinY import LayerSinY
-import numpy as np
+from .BasicLines import BasicLines
 def CreateLines(params):
 
-    Nx_sq = params["Nx_sq"]
-    Ny_sq = params["Ny_sq"]
-    r     = params["r"]
-    type = params["type"]
     # ==================
-    Nx_sq_db = 2*Nx_sq
-    Ny_sq_db = 2*Ny_sq
 
-    Nx = 3*Nx_sq_db
-    Ny = 3*Nx_sq_db
-
-    Lx = 2*r*Nx_sq_db
-    Ly = 2*r*Ny_sq_db
-
-    trajs_X = LayerX(Lx, Ly, Nx)
-    trajs_Y = LayerY(Lx, Ly, Ny)
-
-    S_params = {'Nx'  : Nx_sq_db,
-                'Ny'  : Ny_sq_db,
-                'type': type,
-                "r"   : r}
-
-    trajs_SX = LayerSinX(**S_params) 
-    trajs_SY = LayerSinY(**S_params)
+    trajs_basic = BasicLines(params)
+    trajs_X  = trajs_basic["trajs_X"]
+    trajs_Y  = trajs_basic["trajs_Y"]
+    trajs_SX = trajs_basic["trajs_SX"]
+    trajs_SY = trajs_basic["trajs_SY"]
+    # 
+    Lx = trajs_basic["Lx"]
+    Ly = trajs_basic["Ly"]
 
     # Patrones de diseño
     design_A = [trajs_X]
